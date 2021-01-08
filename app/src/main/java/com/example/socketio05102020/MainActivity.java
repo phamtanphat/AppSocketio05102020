78d5ee00fc18ad16ee5fccb7f9b2b4794c8bdfd9
phatdroid94 <phatdroid94@gmail.com>
2021-01-08 20:11:53 +0700

    client on

--- app/src/main/java/com/example/socketio05102020/MainActivity.java
+++ app/src/main/java/com/example/socketio05102020/MainActivity.java
@@ -3,10 +3,15 @@ package com.example.socketio05102020;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.net.URISyntaxException;
 import io.socket.client.IO;
 import io.socket.client.Socket;
+import io.socket.emitter.Emitter;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -14,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
     private Socket mSocket;
     {
         try {
-            mSocket = IO.socket("http://chat.socket.io");
+            mSocket = IO.socket("http://172.16.1.68:3000");
         } catch (URISyntaxException e) {}
     }
     @Override
@@ -22,5 +27,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mSocket.connect();
+
+        mSocket.on("statusconnect", new Emitter.Listener() {
+            @Override
+            public void call(Object... args) {
+                try {
+                    Object object = args[0];
+                    JSONObject jsonObject = new JSONObject(object.toString());
+                    String message = jsonObject.getString("message");
+                    Log.d("BBB",message);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
